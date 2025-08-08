00456583225b3993e50f5def4c8462970c197e3f
vipin kumar <vipinsinghjnv@gmail.com>
2025-08-08 08:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("hamburger-show").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("phone-nav").classList.toggle("show");
+});
+
+document.getElementById("hamburger-close").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("phone-nav").classList.toggle("show");
+});
