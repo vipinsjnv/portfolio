cacc159f1f7fa3c116acf537b22fc0aea458c0fa
vipin kumar <vipinsinghjnv@gmail.com>
2025-08-14 00:18:29 +0530

    made the sub sections responsive

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.getElementById("hamburger-close").addEventListener("click", (e) => {
   e.preventDefault();
   document.getElementById("phone-nav").classList.toggle("show");
 });
+
+const buttons = document.querySelectorAll(".phone__navbar-link");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    document.getElementById("phone-nav").classList.toggle("show");
+  });
+});
